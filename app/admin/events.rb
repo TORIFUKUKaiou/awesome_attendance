22fda93dae485a5b70307f823b3e8e5fbf2089cb
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2019-07-24 17:36:47 +0900

    change csv filename

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -2,14 +2,15 @@ ActiveAdmin.register Event do
   permit_params :name, :date, :place_id
 
   member_action :download_attendance, method: :get do
+    event = Event.find(params[:id])
     result_csv = CSV.generate do |csv|
       csv << %w[大学名 氏名 ふりがな 出欠]
-      event = Event.find(params[:id])
       Attendance.includes(:user).where(event: event).order('users.org_id').each do |a|
         csv << [a.user.org.name, a.user.name, '', '〇']
       end
     end
-    send_data result_csv, disposition: 'attachment; filename=report.csv'
+    filename = "#{event.date.strftime('%Y%m%d')}_#{event.name}_#{event.place.name}_attendance.csv"
+    send_data result_csv, disposition: "attachment; filename=#{filename}"
   end
 
   index do
