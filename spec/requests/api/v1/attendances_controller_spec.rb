12dd6da89835daa92d16886a8dc08fedd7f2af78
Ryota Fukata <fukata@haw.co.jp>
2019-08-22 11:24:29 +0900

    improve attendances rspec to token auth

--- spec/requests/api/v1/attendances_controller_spec.rb
+++ spec/requests/api/v1/attendances_controller_spec.rb
@@ -4,21 +4,22 @@ RSpec.describe Api::V1::AttendancesController, type: :request do
   describe 'POST /api/v1/attendances' do
     let(:params) do
       {
-        attendance: { code: user.code, place_id: place.id }
+          attendance: { code: user.code, place_id: place.id }
       }
     end
     let(:place) { FactoryBot.create(:place) }
     let(:org) { FactoryBot.create(:org) }
     let(:user) { FactoryBot.create(:user, org: org) }
     let(:event) { FactoryBot.create(:event, place: place) }
-
+    let(:token) { Token.create(label:"name", key:"secret") }
+    let(:headers) { { 'Authorization' => "Bearer #{token.key}" } }
     before do
       event
     end
 
     context 'when success' do
-      before { post api_v1_attendances_path(format: :json), params: params }
-
+      before { post api_v1_attendances_path(format: :json), params: params, headers: headers }
+      
       it { expect(response.status).to eq 200 }
     end
   end
