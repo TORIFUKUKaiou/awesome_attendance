6b07d52538da5027c06b97ea068f1a86265a962e
Ryota Fukata <fukata@haw.co.jp>
2019-08-22 15:12:44 +0900

    refactoring token index of active_admin

--- app/admin/tokens.rb
+++ app/admin/tokens.rb
@@ -1,16 +1,6 @@
 ActiveAdmin.register Token do
   permit_params :label, :key
 
-  index do
-    selectable_column
-    id_column
-    column :label
-    column :digest_hash
-    column :created_at
-    column :updated_at
-    actions
-  end
-
   form do |f|
     f.inputs do
       f.inputs :label
